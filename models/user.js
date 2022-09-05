3416fc08f15759c8737156428d6e7c8345a5e33d
Uwem Uke <slyart147@gmail.com>
2022-09-05 18:33:09 +0100

    drop unique index

--- models/user.js
+++ models/user.js
@@ -3,48 +3,29 @@ var passportLocalMongoose = require('passport-local-mongoose');
 
 var Schema = mongoose.Schema;
 
-var subscriptionSchema = new Schema({
-    plan: {
-      type: String, 
-      required: true
-    },
-    value: {
-      type: Number, 
-      required: true
-    },
-    session_per_month: {
-      type: Number,
-      required: true 
-    }
-  },
-  {
-    timestamps: true
-  });
 
 var User = new Schema({
-    firstname: {
-            type: String,
-            default: ''
-    },
-    lastname: {
-        type: String,
-        default: ''
-    },
-    email: {
-        type: String,
-        default: ''
-    },
-    username: String,
-    password: String,
-    admin:   {
-        type: Boolean,
-        default: false
-    },
-    subscription: [subscriptionSchema],
-    new_subscription: {
-      type: Schema.Types.ObjectId,
-      ref: 'Subscription'
-    }
+  firstname: {
+          type: String,
+          default: ''
+  },
+  lastname: {
+      type: String,
+      default: ''
+  },
+  email: {
+      type: String,
+      default: ''
+  },
+  username: String,
+  password: String,
+  admin:   {
+      type: Boolean,
+      default: false
+  },
+},
+{
+  timestamps: true
 });
 
 User.plugin(passportLocalMongoose);
--- routes/subscription.js
+++ routes/subscription.js
@@ -44,73 +44,61 @@ router.get('/get_subscription',  cors.corsWithOptions, authenticate.verifyUser, 
   });
 });
 
-router.post('/subscribe', cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
-  Subscription.findOne({user: req.user._id})
-  .then((sub) => {
-    if(sub === null){
-      let body = {
+router.put('/subscribe', cors.corsWithOptions, authenticate.verifyUser, async (req, res) => {
+
+  try {
+    let doc = await Subscription.findOne({ user: req.user._id });
+
+    if(doc === null) {
+
+      const new_doc = new Subscription({
         plan: req.body.plan,
         price: req.body.value,
         sessions_per_month: req.body.sessions,
         sessions_scheduled: 0,
         user: req.user._id
-      }
-      Subscription.create(body)
-      .then((new_sub) => {
-        let new_body = {
-          plan: new_sub.plan,
-          price: new_sub.price,
-          sessions_per_month: new_sub.sessions_per_month,
-          sessions_scheduled: new_sub.sessions_scheduled,
-          isSubscribed: true
-        }
-
-        res.statusCode = 201;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(new_body);
-      })
-      .catch((err) => {
-          res.statusCode = 500;
-          res.setHeader('Content-Type', 'application/json');
-          res.json(err);
       });
+
+      await new_doc.save();
+
+      let new_body = {
+        plan: new_doc.plan,
+        price: new_doc.price,
+        sessions_per_month: new_doc.sessions_per_month,
+        sessions_scheduled: new_doc.sessions_scheduled,
+        isSubscribed: true
+      }
+
+      res.statusCode = 201;
+      res.setHeader('Content-Type', 'application/json');
+      return res.json(new_body);
     }
     else {
-      let update = {
-        plan: req.body.plan,
-        price: req.body.value,
-        sessions_per_month: req.body.sessions,
-        sessions_scheduled: 0,
-      }
-      Subscription.findOneAndUpdate({user: req.user._id}, update, {
-        new: true,
-        useFindAndModify: false
-      })
-      .then((new_sub) => {
-        let new_body = {
-          plan: new_sub.plan,
-          price: new_sub.price,
-          sessions_per_month: new_sub.sessions_per_month,
-          sessions_scheduled: new_sub.sessions_scheduled,
-          isSubscribed: true
-        }
 
-        res.statusCode = 201;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(new_body);
-      })
-      .catch((err) => {
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(err);
-      });
+      doc.plan = req.body.plan
+      doc.price = req.body.value
+      doc.sessions_per_month = req.body.sessions
+      doc.sessions_scheduled = 0
+      await doc.save();
+
+      let update = {
+        plan: doc.plan,
+        price: doc.value,
+        sessions_per_month: doc.sessions_per_month,
+        sessions_scheduled: doc.sessions_scheduled,
+        isSubscribed: true
+      }
+
+      res.statusCode = 201;
+      res.setHeader('Content-Type', 'application/json');
+      return res.json(update);
     }
-  })
-  .catch((err) => {
+  }
+  catch(error){
     res.statusCode = 500;
     res.setHeader('Content-Type', 'application/json');
-    res.json(err);
-  });
+    res.json(error);
+  }
 });
 
 
